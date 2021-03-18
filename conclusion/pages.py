d339710cda87991b21435dbcccfccbe7d9bc37e2
DamienMAYAUX <mayaux.damien@gmail.com>
2021-03-18 13:09:47 +0100

    Main files

---
+++ conclusion/pages.py
@@ -0,0 +1,14 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+class LastPage(Page):
+    form_model = 'player'
+    form_fields = ['feedback']
+    
+    def vars_for_template(self):
+        return {'payoff_normalized' : float(self.player.participant.payoff)/100}
+
+
+
+page_sequence = [LastPage]
