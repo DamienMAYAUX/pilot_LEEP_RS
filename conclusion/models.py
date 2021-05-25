e28079e76fb9b7828e5fc922509aabb4c037c19c
DamienMAYAUX <mayaux.damien@gmail.com>
2021-05-25 17:29:08 +0200

    Blank field conclusion
    Allowing blank field in conclusion

--- conclusion/models.py
+++ conclusion/models.py
@@ -31,7 +31,7 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     # feedback = models.LongStringField(label = "Feel free to leave us a feedback on the experiment ")
-    feedback = models.LongStringField( label = """N'hésitez pas à donner ci-dessous votre ressenti\
+    feedback = models.LongStringField( blank = True, label = """N'hésitez pas à donner ci-dessous votre ressenti\
                                       sur l'expérience,\n à expliquer comment vous avez procédé ou à\
                                           partager vos intuitions\n sur la pertinence de la recommandation.""")
     minus5000done = models.BooleanField(initial=False)
